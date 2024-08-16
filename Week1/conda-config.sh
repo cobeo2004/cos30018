2d207017bad150495c346a44cc256d4065428160
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-08-16 11:23:17 +1000

    chore: add example for configurating conda

---
+++ Week1/conda-config.sh
@@ -0,0 +1,11 @@
+# Example for configurating conda environment
+# Author: Simon Nguyen
+# Date: 2024-02-15
+
+# Generate conda environment with the name of cos30018-env-w1-v0.1
+# using python 3.11 (You can change this to any name after -n flag you want)
+# You can change the name after -n flag to your preffered name
+# You can change the version of python after python= flag to your preffered version
+conda create -n cos30018-env-w1-v0.1 python=3.11
+# Activate the environment
+conda activate cos30018-env-w1-v0.1
