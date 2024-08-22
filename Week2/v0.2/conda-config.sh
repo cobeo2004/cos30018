56025aeabc732b936c23b8fc9d4ae05bc162a4f6
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-08-22 21:35:02 +1000

    feat: Add Week 2 content

---
+++ Week2/v0.2/conda-config.sh
@@ -0,0 +1,6 @@
+conda create -n cos30019_env_w2_v0.2 python=3.10.9
+conda activate cos30019_env_w2_v0.2
+#Check current environment
+conda info --envs
+#Check current python version
+python --version
---
+++ Week2/v0.2/pip-config.sh
@@ -0,0 +1,4 @@
+#Download TA-lib
+conda install -c conda-forge ta-lib
+#Download all dependencies
+pip install -r requirements.txt
